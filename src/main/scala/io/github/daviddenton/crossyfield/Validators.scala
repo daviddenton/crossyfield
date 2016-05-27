08774f12a85548b93f13f158bfc58c177e49155c
David Denton <denton.david@gmail.com>
2016-05-27 19:48:05 +0100

    dave - docs

--- src/main/scala/io/github/daviddenton/crossyfield/Validators.scala
+++ src/main/scala/io/github/daviddenton/crossyfield/Validators.scala
@@ -1,6 +1,13 @@
 package io.github.daviddenton.crossyfield
 
+/**
+  * Convienience Validators
+  */
 object Validators {
+
+  /**
+    * Converting to various primitive types from a String
+    */
   object string {
     val optional = new PrimitiveValidators(false)
     val required = new PrimitiveValidators(true)
--- src/test/scala/examples/CollectErrors.scala
+++ src/test/scala/examples/CollectErrors.scala
@@ -2,21 +2,20 @@ package examples
 
 import java.time.LocalDate
 
-import io.github.daviddenton.crossyfield.{Ignored, Validation, Validator}
+import io.github.daviddenton.crossyfield.{Validation, Validators}
 
 object CollectErrors extends App {
 
   /**
-    * Provides an Optional validator
+    * Provides an optional validator using the convenience methods
     */
-  def dateValidator(id: Symbol) = Validator.mk(id) {
-    in: String =>
-      if (in.isEmpty) Ignored
-      else Validator.mk(id, "invalid date", LocalDate.parse) <--? in
-  }
+  def dateValidator(id: Symbol) = Validators.string.optional.localDate(id)
 
   val millennium = LocalDate.of(2000, 1, 1)
 
+  /**
+    * Because we are interested in collecting ALL of the errors, we can't use cross-field validation here
+    */
   val errors = Validation.collectErrors(
     dateValidator('theFuture) <--?("2000-01-01", "must be after the millennium", _.isAfter(millennium)),
     dateValidator('anyOldDate) <--? "ASDA-01-01",
