4fe9b7ce4c9658813f253782ece06587f859e36e
David Denton <denton.david@gmail.com>
2016-05-30 17:58:21 +0100

    dave - collect

--- src/test/scala/examples/Magnet.scala
+++ src/test/scala/examples/Magnet.scala
@@ -4,10 +4,10 @@ import scala.language.implicitConversions
 
 sealed trait ExtractedValue[T]
 
-class Args2[C](implicit desired: Manifest[C]) {
-  def unapply[X](c: X)(implicit m: Manifest[X]): Option[C] = {
+class Args2[T](desired: Manifest[T]) {
+  def unapply[X](c: X)(implicit m: Manifest[X]): Option[T] = {
     def sameArgs = desired.typeArguments.zip(m.typeArguments).forall { case (d, actual) => d >:> actual }
-    if (desired >:> m && sameArgs) Some(c.asInstanceOf[C])
+    if (desired >:> m && sameArgs) Some(c.asInstanceOf[T])
     else None
   }
 }
@@ -16,27 +16,30 @@ case class Extracted[T](t: T) extends ExtractedValue[T]
 
 case class Missing[T]() extends ExtractedValue[T]
 
+abstract class Bob[A](desired: Manifest[A]) extends Function[PartialFunction[A, String], Function[A, String]] {
+  val Args = new Args2[A](desired)
+}
+
 class Magnet[A] {
-  def apply(): Function[PartialFunction[A, String], Function[A, String]] = pf => in => pf(in)
+  def apply(desired: Manifest[A]): Bob[A] = new Bob[A](desired) {
+    override def apply(pf: PartialFunction[A, String]): Function[A, String] = (in: A) => pf(in)
+  }
 }
 
 object Magnet {
 
-  implicit def t2ToMagnet[A, B](a: (A, B)): Magnet[(A, B)] = new Magnet[(A, B)]
+  def apply[A](magnet: Magnet[A])(implicit desired: Manifest[A]) = magnet.apply(desired)
 
-  implicit def t3ToMagnet[A, B, C](a: (A, B, C)): Magnet[(A, B, C)] = new Magnet[(A, B, C)]
+  implicit def tupleToMagnet[T](a: (T)): Magnet[T] = new Magnet[T]
 }
 
 object MagnetApp extends App {
 
-  def doIt[A](magnet: Magnet[A]) = magnet.apply()
-
-  val a = new Args2[(Int, Int)]()
-  private val it1 = doIt(1, 3)
-  private val it = it1 {
-    case a(a, b) => "asd " + a + b
+  private val it1 = Magnet(1)
+  private val out = it1 {
+    case it1.Args(a, b) => "asd " + a + b
   }
 
-  println(it(1, 2))
+  println(out(1, 2))
 
 }
