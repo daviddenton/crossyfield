41333ad2c48df2b84dc9e34e72d8018a2763967f
David Denton <denton.david@gmail.com>
2016-06-10 18:56:13 +0100

    removed mad reflectoporn

--- src/test/scala/examples/Magnet.scala
+++ src/test/scala/examples/Magnet.scala
@@ -4,18 +4,19 @@ import io.github.daviddenton.crossyfield.{Extracted, Extraction, Extractors, Inv
 
 import scala.language.implicitConversions
 
-class Validation[In <: Product](extractors: Product, fn: () => In) {
+class Validation[In <: Product] private(extractors: Product, value: => In) {
   private val errors = extractors.productIterator.filter(_.isInstanceOf[Extraction[_]]).map(_.asInstanceOf[Extraction[_]]).toList.flatMap {
     case Invalid(q) => q
     case _ => Nil
   }
 
-  def apply[Result](pf: PartialFunction[In, Result]): Extraction[Result] = if (errors.isEmpty) Extracted(pf(fn())) else Invalid(errors)
+  def apply[Result](pf: Function[In, Result]): Extraction[Result] = if (errors.isEmpty) Extracted(pf(value)) else Invalid(errors)
 }
 
 object Validation {
 
   type E[T] = Extraction[T]
+  type O[T] = Option[T]
 
   private def getFrom[T](e: E[T]): Option[T] = e match {
     case Extracted(v) => Some(v)
@@ -25,9 +26,9 @@ object Validation {
 
   def apply[In <: Product](validation: Validation[In]) = validation
 
-  implicit def t2ToMagnet[A, B](in: (E[A], E[B])): Validation[(Option[A], Option[B])] = new Validation(in, () => (getFrom(in._1), getFrom(in._2)))
+  implicit def t2ToMagnet[A, B](in: (E[A], E[B])): Validation[(O[A], O[B])] = new Validation(in, (getFrom(in._1), getFrom(in._2)))
 
-  implicit def t3ToMagnet[A, B, C](in: (E[A], E[B], E[C])): Validation[(Option[A], Option[B], Option[C])] = new Validation(in, () => (getFrom(in._1), getFrom(in._2), getFrom(in._3)))
+  implicit def t3ToMagnet[A, B, C](in: (E[A], E[B], E[C])): Validation[(O[A], O[B], O[C])] = new Validation(in, (getFrom(in._1), getFrom(in._2), getFrom(in._3)))
 }
 
 
@@ -36,9 +37,11 @@ object MagnetApp extends App {
   private val int = Extractors.string.optional.int('asd)
   private val bbb = Extractors.string.required.int('asd2)
 
-  Validation(int <--? "123", bbb <--? "321") {
-    case (a, b) => a.get + b.get
-  } match {
+  val e = Validation(int <--? "123", bbb <--? "321") {
+    case (a, b) => "as " + a.get + b.get
+  }
+
+  e match {
     case Extracted(b) => println(b)
     case NotProvided => println("nothing")
     case Invalid(i) => println(i)
