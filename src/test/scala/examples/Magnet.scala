6a318cd92f9f68df151407fec1ba8734e672903c
David Denton <denton.david@gmail.com>
2016-06-10 16:27:21 +0100

    removed mad reflectoporn

--- src/test/scala/examples/Magnet.scala
+++ src/test/scala/examples/Magnet.scala
@@ -1,5 +1,7 @@
 package examples
 
+import io.github.daviddenton.crossyfield.{Extraction, Extractors, Invalid}
+
 import scala.language.implicitConversions
 
 sealed trait ExtractedValue[T]
@@ -13,6 +15,11 @@ class Magnet[In](in: In) {
 
 object Magnet {
 
+  def toErrors(tuple: Product) = tuple.productIterator.map(_.asInstanceOf[Extraction[_]]).toList.flatMap {
+    case Invalid(q) => q
+    case _ => Nil
+  }
+
   def apply[In](magnet: Magnet[In])(implicit desired: Manifest[In]) = magnet.apply(desired)
 
   implicit def t2ToMagnet[A, B](in: (A, B)): Magnet[(A, B)] = new Magnet[(A, B)](in)
@@ -20,7 +27,35 @@ object Magnet {
   implicit def t3ToMagnet[A, B, C](in: (A, B, C)): Magnet[(A, B, C)] = new Magnet[(A, B, C)](in)
 }
 
+
+trait ExtractionM[T <: Product] {
+
+}
+
+object ExtractionM {
+
+  def apply[In <: Product](magnet: ExtractionM[In])(implicit desired: Manifest[In]) = ???
+
+  private def toErrors(tuple: Product) = tuple.productIterator.map(_.asInstanceOf[Extraction[_]]).toList.flatMap {
+    case Invalid(q) => q
+    case _ => Nil
+  }
+
+  implicit def t2ToMagnet[A, B](in: (Extraction[A], Extraction[B])): ExtractionM[(A, B)] = new ExtractionM[(A, B)] {
+
+  }
+
+  implicit def t3ToMagnet[A, B, C](in: (A, B, C)): Magnet[(A, B, C)] = new Magnet[(A, B, C)](in)
+}
+
+
 object MagnetApp extends App {
+
+  private val int = Extractors.string.required.int('asd)
+  private val bbb = Extractors.string.required.int('asd2)
+
+  println(ExtractionM((int <--? "123", bbb <--? "321")))
+
   private val it1 = Magnet(Option(1), Option(3))
   private val out = it1 {
     case (a, b) => "asd " + a + b
