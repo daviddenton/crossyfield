be1498cc8ecd3abbdbb0710e799ef1b3da70b844
David Denton <denton.david@gmail.com>
2016-06-11 20:36:17 +0100

    dave -revert: added Validator

--- src/main/scala/io/github/daviddenton/crossyfield/Validator.scala
+++ src/main/scala/io/github/daviddenton/crossyfield/Validator.scala
@@ -2,20 +2,6 @@ package io.github.daviddenton.crossyfield
 
 import scala.language.implicitConversions
 
-sealed trait Validation[+T]
-
-case class Validated[T](value: T) extends Validation[T]
-
-/**
-  * Represents a object which could not be extracted due to it being invalid or missing when required.
-  */
-case class ValidationFailed(errors: Seq[Error]) extends Validation[Nothing]
-
-object ValidationFailed {
-  def apply(p: Error): ValidationFailed = ValidationFailed(Seq(p))
-}
-
-
 class Validator[In <: Product] private(extractors: Product, value: => In) {
   private val errors = extractors
     .productIterator
